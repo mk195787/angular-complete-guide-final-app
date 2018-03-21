b6e61668e492a3bce8677c2ab7c3a9432e16b17a
Marcin Kielbasinski <marcin.kielbasinski@syncron.com>
2018-03-21 10:01:31 +0100

    Recipe model + markup of items

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe('Name', 'Desc', 'http://ultalife.co/blog/wp-content/uploads/2017/10/recipe-icon-150x150.jpeg'),
+    new Recipe('Name', 'Desc', 'http://ultalife.co/blog/wp-content/uploads/2017/10/recipe-icon-150x150.jpeg')
+  ];
 
   constructor() { }
 

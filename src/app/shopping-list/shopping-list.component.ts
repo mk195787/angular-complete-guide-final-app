17d1dab9e4be767966fbc4d735c45ef1e91dd6a1
Marcin Kielbasinski <marcin.kielbasinski@syncron.com>
2018-03-26 16:17:43 +0200

    Cleaning + Observable

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,27 @@
 import { ShoppingService } from './shopping.service';
 import { Ingredient } from './../shared/ingredient.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
   ingredients: Ingredient[];
 
   constructor(private shoppingService: ShoppingService) {}
 
   ngOnInit() {
-    this.shoppingService.ingredientAdded.subscribe(
+    this.subscription = this.shoppingService.ingredientAdded.subscribe(
       (ings: Ingredient[]) => this.ingredients = ings
     );
     this.ingredients = this.shoppingService.getIngredients();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping.service.ts
+++ src/app/shopping-list/shopping.service.ts
@@ -1,8 +1,9 @@
 import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 export class ShoppingService {
-    ingredientAdded = new EventEmitter<Ingredient[]>();
+    ingredientAdded = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Carrot', 3),
@@ -11,12 +12,12 @@ export class ShoppingService {
 
     addIngredient(ing: Ingredient) {
         this.ingredients.push(ing);
-        this.ingredientAdded.emit(this.ingredients);
+        this.ingredientAdded.next(this.ingredients);
     }
 
     addIngredients(ings: Ingredient[]) {
         this.ingredients = this.ingredients.concat(ings);
-        this.ingredientAdded.emit(this.ingredients);
+        this.ingredientAdded.next(this.ingredients);
     }
 
     getIngredients() {
